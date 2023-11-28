684f6dcb7e7a01ff042e69d2195bdd394ae0ad67
Qumber Rizvi <qumberrizvi@ymail.com>
2023-11-28 09:16:33 +0530

    change routes

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-
-Route::group(['prefix' => 'admin'], function () {
-    Voyager::routes();
-});
+Voyager::routes();
