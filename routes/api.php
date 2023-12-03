261d444413953363ab6ea91b2510963c6d2f8089
Qumber Rizvi <qumberrizvi@ymail.com>
2023-12-04 03:56:16 +0530

    API routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +18,16 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::group(['namespace' => 'App\Http\Controllers\Api'], function () {
+    Route::get('posts/search', [PostController::class, 'search']);
+    Route::apiResource('posts', PostController::class, [
+        'only' => ['index', 'show']
+    ]);
+
+    Route::get('categories/{slug}/posts', [PostController::class, 'indexByCategory']);
+    Route::apiResource('categories', CategoryController::class, [
+        'only' => ['index', 'show']
+    ]);
 });
